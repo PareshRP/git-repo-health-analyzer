16b9fc09a830f501354847c94b85c9e1e2ebbe97
Paresh Patil <patilparesh100@gmail.com>
2026-01-29 00:38:17 +0530

    fix: use absolute path for sqlite db

--- app/storage/database.py
+++ app/storage/database.py
@@ -1,6 +1,8 @@
 import sqlite3
+import os
 
-DB_NAME = "cache.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "../../cache.db")
 
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+    return sqlite3.connect(DB_PATH)
